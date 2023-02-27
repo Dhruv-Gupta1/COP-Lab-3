da7f87a6019d1ce67452774b796c5bbc4d05c90b
Dhruv-Gupta1 <96294191+Dhruv-Gupta1@users.noreply.github.com>
2023-02-27 15:08:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add new post to the list of recent posts
+function addPost(title, text, author) {
+    var posts = document.getElementById("posts");
+    var post = document.createElement("li
+  
